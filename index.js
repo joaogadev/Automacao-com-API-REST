93b280c28a4c97b13a3c85331fea878deb890df7
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-08-07 01:40:39 -0300

    feat: cria json provisionamento com array de obejtos endereco e slug

--- index.js
+++ index.js
@@ -48,6 +48,63 @@ async function buscarRegistroPorCep(cepBuscado) {
     }
 }
 
+function makeSlug(text) {
+    return text.normalize("NFD") //separa letras de acentos
+        .replace(/[\u0300-\u036f]/g, "") //tira acentos
+        .toLowerCase() 
+        .trim()
+        .replace(/\s+/g, "-") //substitui espaços por hífens
+        .replace(/[^\w\-]+/g, "") //remove caracteres especiais
+        .replace(/\-\-+/g, "-"); //remove hífens duplicados
+}
+
+async function criarJsonFinal(cepBuscado) {
+    try {
+        let dataJsonOriginal = escreverJson("consulta.csv");
+
+        let getLogradouro = await buscarRegistroPorCep(cepBuscado).then(data => data.logradouro);
+
+        console.log(getLogradouro);
+
+        let getBairro = await buscarRegistroPorCep(cepBuscado).then(data => data.bairro);
+
+        console.log(getBairro);
+
+        const item = dataJsonOriginal.find(
+            item => item.cep.trim() === String(cepBuscado).trim()
+        );
+
+        const serv = dataJsonOriginal.find(
+            item => item.cep.trim() === String(cepBuscado).trim()
+        ).servicos;
+        
+        item.slug = makeSlug(item.nome);
+
+        item.endereco = {
+            logradouro: getLogradouro,
+            bairro: getBairro,
+            cidade: item.cidade,
+            uf: item.uf,
+            cep: item.cep
+        };
+
+        delete item.cidade;
+        delete item.uf;
+        delete item.cep;
+
+        const final = fs.writeFileSync(
+            "posicionamento.json",
+            JSON.stringify(dataJsonOriginal, null, 2),
+            "utf8"
+        );
+
+        return JSON.parse(fs.readFileSync("posicionamento.json", "utf8"));
+
+    } catch (err) {
+        console.error("Não foi possível buscar o registro por logradouro e bairro:", err.message);
+    }
+}
+
 async function main() {
     let cep = "49010-390";
     console.log(await criarJsonFinal(cep));
